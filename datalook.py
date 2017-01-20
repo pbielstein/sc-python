c78a93d6a8e2a083995464d1421a49a6a76a0bac
pbielstein <patrick.bielstein@gmail.com>
2017-01-20 12:17:51 +0000

    Update datalook.py

--- datalook.py
+++ datalook.py
@@ -1,4 +1,4 @@
-""" code to take a look at the data """
+"""Code to take a look at the data."""
 # coding: utf-8
 
 import sys
@@ -7,15 +7,15 @@ import matplotlib.pyplot
 
 
 def analyse(filename, outfile=None):
-    """This function loads a file and plots the average, maximum, and minimum across columns"""
+    """Function loads a file and plots the average, maximum, and minimum across columns."""
     data = numpy.loadtxt(fname = filename, delimiter = ',')
     # create a wide figure to hold the subplots
-    fig = matplotlib.pyplot.figure (figsize =(10.0, 3.0))
+    fig = matplotlib.pyplot.figure(figsize =(10.0, 3.0))
     
     # Create placeholders for plots
-    subplot1 = fig.add_subplot (1,3,1)
-    subplot2 = fig.add_subplot (1,3,2)
-    subplot3 = fig.add_subplot (1,3,3)
+    subplot1 = fig.add_subplot(1,3,1)
+    subplot2 = fig.add_subplot(1,3,2)
+    subplot3 = fig.add_subplot(1,3,3)
     
     subplot1.set_ylabel('average')
     subplot1.plot(numpy.mean(data, axis=0))
@@ -34,12 +34,9 @@ def analyse(filename, outfile=None):
         
 
 def detect_problems (filename):
+        """Some of our temperature files have problems, check for these. This function reads a file (filename argument) and reports on odd looking maxima and minima that add up to
+        zero. This seems to happen when the sensors break. The function does not return any data.""" 
     data = numpy.loadtxt(fname = filename, delimiter = ',')
-    """Some of our temperature files have problems, check for these
-    
-         This function reads a file (filename argument) and reports on odd looking maxima and minima that add up to
-        zero. This seems to happen when the sensors break. The function does not return any data.
-    """ 
     if numpy.max(data, axis=0)[0] == 0 and numpy.max (data, axis=0)[20] == 20:
         print ("Suspicious looking maxima")
     elif numpy.sum(numpy.min(data, axis = 0)) == 0:
@@ -49,9 +46,7 @@ def detect_problems (filename):
 
 # A module can discover whether or not it is running in the main scope by checking its own __name__, which allows a common idiom for conditionally executing code in a module when it is run as a script or with python -m but not when it is imported:      
 if __name__ == "__main__":        
-        
-    print("Running ", sys.argv[0])
-        
+    print("Running ", sys.argv[0])  
     print(sys.argv[1])
     analyse(sys.argv[1], outfile = sys.argv[2])
     detect_problems(sys.argv[1])
